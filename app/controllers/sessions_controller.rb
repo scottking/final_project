fafa10fa34674c7b54d6bb2ce6ffb8a2a063c6f7
Scott King <scott_king@taylor.edu>
2012-12-08 12:38:01 -0500

    Board failures down to 2

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -11,7 +11,7 @@ class SessionsController < ApplicationController
 	  flash[:success] = "Welcome"
       redirect_back_or user
     else
-      flash[:error] = "Invalid"
+      flash[:error] = "Invalid WRONG password/email"
       render 'new'
     end
   end
